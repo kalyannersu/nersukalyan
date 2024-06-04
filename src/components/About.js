d69d3504ad626059841fe6ceb2316445acc91f9c
kalyannersu <58869228+kalyannersu@users.noreply.github.com>
2024-06-04 20:31:44 +0530

    Updated all the styles and code modified names

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+// About.js
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Me</h1>
+      {/* Add your about content here */}
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Services.js
@@ -0,0 +1,13 @@
+// About.js
+import React from 'react';
+
+const Service = () => {
+  return (
+    <div>
+      <h1>About Me</h1>
+      {/* Add your about content here */}
+    </div>
+  );
+};
+
+export default Service;
