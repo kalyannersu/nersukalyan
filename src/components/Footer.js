2a76c9060595e7c73139c188f40840622b316d7c
kalyannersu <58869228+kalyannersu@users.noreply.github.com>
2024-06-04 18:06:46 +0530

    replaced name

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer>
-      <div className="logo"><a href="#">eraf</a></div>
+      <div className="logo"><a href="#">Nersu Kalyan</a></div>
       <div className="btn">instagram</div>
     </footer>
   );
